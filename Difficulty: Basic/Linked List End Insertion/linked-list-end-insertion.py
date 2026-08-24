769731ca81e91df3a1b8ef2cff36d38e0ee6e91a
HinduDusarla017 <hindudusarla@gmail.com>
2026-08-24 12:24:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Linked List End Insertion/linked-list-end-insertion.py
@@ -0,0 +1,19 @@
+'''    
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def insertAtEnd(self, head, x):
+        #code here 
+        curr=head
+        new=Node(x)
+        if head is None:
+            return new
+        
+        while curr.next:
+            curr=curr.next
+        curr.next=new
+        return head
