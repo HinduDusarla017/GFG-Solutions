1b640824883333e6a112a2afea0d9f8d8533c91f
HinduDusarla017 <hindudusarla@gmail.com>
2026-08-26 20:19:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First n Fibonacci using DP/first-n-fibonacci-using-dp.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fibonacciNumbers(self, n):
+        # code here
+        dp=[0]*(n+1)
+        if n>=1:
+            dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=(dp[i-1]+dp[i-2])%(10**9+7)
+        return dp
+            
